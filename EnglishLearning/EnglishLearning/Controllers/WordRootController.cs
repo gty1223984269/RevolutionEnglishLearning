0d413586602ecdd68fd36e8e750d6fd9a3472239
gty1223984269 <1223984269@qq.com>
2018-10-13 17:30:01 +0800

    add swagger

---
+++ EnglishLearning/EnglishLearning/Controllers/WordRootController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using EnglishLearningDomain.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+namespace EnglishLearning.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/WordRoot")]
+
+    public class WordRootController : Controller
+    {
+        private readonly EnglishLearningDbContext _englishLearningDbContext;
+        public readonly int pageSize = 10;
+
+        public WordRootController(EnglishLearningDbContext englishLearningDbContext)
+        {
+            _englishLearningDbContext = englishLearningDbContext;
+        }
+
+
+        [HttpGet]
+        [Route("AllWordRoots")]
+        public List<WordRoots> getWordRoots(int pageNumber)
+        {
+             int skipIndex = pageSize * (pageNumber - 1);
+
+            return _englishLearningDbContext.WordRoots?.Skip(skipIndex).Take(pageSize).ToList();
+        }
+
+
+        [HttpGet]
+        [Route("AllRelatedWordByRootId")]
+        public List<RelatedWords> getRelatedWord(int wordRootId)
+        {
+            return _englishLearningDbContext.RelatedWords?.Where(a => a.RootId == wordRootId).ToList();
+        }
+
+    }
+}
+    
+
--- EnglishLearning/EnglishLearning/Startup.cs
+++ EnglishLearning/EnglishLearning/Startup.cs
@@ -12,6 +12,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace EnglishLearning
 {
@@ -29,6 +30,10 @@ namespace EnglishLearning
         {
             services.AddMvc();
             AddDatabase(services);
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info { Title = "My API", Version = "v1" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -38,7 +43,15 @@ namespace EnglishLearning
             {
                 app.UseDeveloperExceptionPage();
             }
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
 
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.), 
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+            });
             app.UseMvc();
         }
 
