9efe3dc229a055d1ff576be83566dbdba4f54dbd
jack.guo <ext.jack.guo@ef.com>
2018-10-15 18:24:57 +0800

    no message

--- FrontEnd/EnglishLearning/js/elmain.js
+++ FrontEnd/EnglishLearning/js/elmain.js
@@ -2,13 +2,14 @@
  Tool.url="http://localhost:62861/api/WordRoot/AllWordRoots?pageNumber=";
  
 function createContent(word,wordmeaning)
- {
+ {	
 	$("#WordContent").append("<div class='boxword'><h3 class='bgWhite'>"+word+"</h3><h4 class='textLeft'>汉语释义:</h4><div class='boxContent'>  <span class='boxContentspan'>"+wordmeaning+"</span></div></div>");
 	 
  }
 Tool.sendGetRequest=function(index)
  {
 	 $.get(Tool.url+index,function(data,status){
+		 $("#WordContent").empty();
 		 $("#wordCount").text("");
 		 $("#wordCount").append(data.pageCount);
 		   
