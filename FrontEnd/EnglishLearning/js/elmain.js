50fd26db80667ec1b06c1fa37cf919012dfcbb7c
jack.guo <ext.jack.guo@ef.com>
2018-10-16 16:30:38 +0800

    add detail index.html

--- FrontEnd/EnglishLearning/js/elmain.js
+++ FrontEnd/EnglishLearning/js/elmain.js
@@ -1,9 +1,9 @@
  var Tool={};
  Tool.url="http://localhost:62861/api/WordRoot/AllWordRoots?pageNumber=";
- 
-function createContent(word,wordmeaning)
+ Tool.address="http://localhost:62861/api/WordRoot/AllRelatedWordByRootId?wordRootId=";
+function createContent(word,wordmeaning,id)
  {	
-	$("#WordContent").append("<div class='boxword'><h3 class='bgWhite'>"+word+"</h3><h4 class='textLeft'>汉语释义:</h4><div class='boxContent'>  <span class='boxContentspan'>"+wordmeaning+"</span></div></div>");
+	$("#WordContent").append("<div class='boxword'><h3 class='bgWhite'><a href=detail.html?wordRootId="+id+"&word="+word+">"+word+"</a></h3><h4 class='textLeft'>汉语释义:</h4><div class='boxContent'>  <span class='boxContentspan'>"+wordmeaning+"</span><span style='display:none' id='wordId'>"+id+"</span></div>");
 	 
  }
 Tool.sendGetRequest=function(index)
@@ -18,9 +18,7 @@ Tool.sendGetRequest=function(index)
 				 $.each(value,function(index,value)
 				 {
 					 console.log(value);
-				var word=value.word;
-				var meaning=value.chineseMeaning;
-				createContent(word,meaning);
+					createContent(value.word,value.chineseMeaning,value.id);
 					 
 				 })
 					  });
@@ -28,3 +26,39 @@ Tool.sendGetRequest=function(index)
 	 
  }
  
+ 
+ function createContentDetail(word,chineseMeaning,id,rememberLogic,wordrootcc)
+  {	
+ 	$("#WordContent").append("<div class='boxword extraHeight'><h3 class='bgWhite'>"+word+"</h3><h4 class='textLeft'>汉语释义:</h4><div class='boxContent reduceHeight'> <span class='boxContentspan'>"+chineseMeaning+"</span></div><h4 class='textLeft'>记忆逻辑:</h4><div class='boxContent'> <span class='boxContentspan'>"+rememberLogic+"</span></div><div></div></div>");
+ 	 
+  }
+ 
+Tool.sendGetRequestForAllWords=function(wordId,wordsb)
+  {
+		
+		$("#wordRootSB").empty();
+		$("#wordRootSB").text("");
+		$("#wordRootSB").append("词根:"+wordsb);
+ 	 $.get(Tool.address+wordId,function(data,status){
+ 			 $.each(data,function(index,value){
+ 					 createContentDetail(value.word,value.chineseMeaning,value.id,value.rememberLogic,wordrootcc);
+ 					  });
+ 	 		
+ 	 
+  });
+ }
+ 
+ 
+ Tool.getAddress=function GetRequest() {
+            var url = location.search; //获取url中"?"符后的字串 
+          url= decodeURI(url);
+            var theRequest = new Object();
+            if (url.indexOf("?") != -1) {
+                var str = url.substr(1);
+                strs = str.split("&");
+                for (var i = 0; i < strs.length; i++) {
+                    theRequest[strs[i].split("=")[0]] = unescape(strs[i].split("=")[1]);
+                }
+            }
+            return theRequest;
+        }
